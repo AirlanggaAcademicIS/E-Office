96150a206c08528f724d6278376ec873632ae51b
IkaRahmawati <ika.rahmawati-13@fst.unair.ac.id>
2016-05-10 20:52:13 +0700

    Add files via upload
    Do : konek database, mengambil data dari database dan ditampilkan dalam tabel, merubah link navbar Index.php agar terhubung dengan checkAgenda.php
    problem : On clik tabel untuk memunculkan keterangan
    Future  : On clik tabel

--- koneksi.php
+++ koneksi.php
@@ -2,7 +2,7 @@
  	$db_host = "localhost";
 	$db_user = "root";
 	$db_password = "";
-	$db_nama = "ivent";
+	$db_nama = "e-office";
 	$koneksi = mysql_connect($db_host, $db_user, $db_password) or die ("gagal konek");
 	$koneksi_db = mysql_select_db($db_nama)or die ("database tidak ditemukan");
 		
