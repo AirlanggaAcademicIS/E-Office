1c11b3e84cefe83950d8f5a81d8e467786cf1be0
VinaRosalia <vinarosalia25@yahoo.com>
2016-05-11 22:17:24 +0700

    Share dari kadep ke dosen

---
+++ Forward.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<form id="form1" name="form1" method="post" action="">
+  <p><strong>Forward</strong></p>
+  <table width="311" border="1">
+    <tr>
+     
+      <td width="200">Kategori</td>
+      <td width="117"><label>
+        <select name="select2">
+          <option value="None">None</option>
+          <option value="Perempuan">Perempuan</option>
+          <option value="Laki-Laki">Laki-Laki</option>
+        </select>
+      </label></td>
+    </tr>
+  </table>
+  <p>Nama Dosen :</p>
+  <p>
+    <label>
+    <input type="checkbox" name="checkbox" value="checkbox" />
+    Indra</label>
+</p>
+  <p align="center">&nbsp;	</p>
+  <p>&nbsp; </p>
+</form>
+
+</body>
+</html>
