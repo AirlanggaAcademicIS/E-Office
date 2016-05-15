f454f83460f5fccee05f6a095d3d1ca54d86fe10
VinaRosalia <vinarosalia25@yahoo.com>
2016-05-15 15:12:18 +0700

    Login
    do : menyempurnakan fitur login ketika benar atau salah memasukkan password dan username
    problem : menyocokkan database
    to do : mengerjakan fitur yg lain

--- login.php
+++ login.php
@@ -1,34 +1,49 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Untitled Document</title>
-
-</head>
-
-<body>
-<table width="1077" border="1">
+<table width="1357" border="0">
   <tr>
-    <td width="646"><img src="../1.jpg" width="646" height="534" /></td>
-    <td width="415"><div align="center">
-      <p><strong>LOGIN</strong></p>
-      <p>
-        <label>
-        <input type="text" name="Username" />
-        </label>
-      </p>
-      <form id="form1" name="form1" method="get" action="">
-        <label>
-          <input type="password" name="Password" />
-          </label>
-        <p>&nbsp;</p>
+    <td width="50%"><img src="../1.jpg" width="646" height="534"></td>
+    <td width="50%"><div align="center">
+      <form action="" method="post">
+        <p><strong>LOGIN</strong></p>
+        <p>
+          <input type="text" name="username" placeholder="Username">
+              </p>
+        <p>
+          <input type="password" name="password" placeholder="Password">
+          </p>
+		  <font color="#FF0000">
+<?php
+session_start();
+include 'koneksi.php';
+ 
+if(!empty($_POST)){
+     
+    $username = $_POST['username'];
+    $password = md5($_POST['password']);
+ 
+    $sql = "select * from signup where username='".$username."' and password='".$password."'";
+    #echo $sql."<br />";
+    $query = mysql_query($sql) or die (mysql_error());
+ 
+    // pengecekan query valid atau tidak
+    if($query){
+        $row = mysql_num_rows($query);
+         
+        // jika $row > 0 atau username dan password ditemukan
+        if($row > 0){
+            $_SESSION['isLoggedIn']=1;
+            $_SESSION['username']=$username;
+            header('Location: index.php');
+        }else{
+            echo "username atau password salah";
+        }
+    }
+}
+?>
+</font>
+        <p>
+          <input type="submit" value="login">
+                        </p>
       </form>
-      <form id="form2" name="form2" method="post" action="home.html">
-        <label>
-          <input type="submit" name="Submit" value="Masuk" />
-          </label>
-        
-        </form>
       <p>&nbsp;</p>
       <p>&nbsp;</p>
       <p>&nbsp;</p>
@@ -36,39 +51,5 @@
       <p>&nbsp;</p>
     </div></td>
   </tr>
-  
-  <?php
-session_start();
  
-if(isset($_POST['Masuk'])){
-
-
-include ("koneksi.php");
-$Username=$_GET['Username'];
-$Password=$_GET['Password'];
-
-$qry="SELECT Masuk FROM karyawan WHERE Username ='$Username'"; 
-$result=mysql_query($qry);
-
-$count=mysql_num_rows($result);
-
-if($count==0){
-	die("Pasword was probably incorect");
-	}
-else if{$count==1){
-$_SESSION['login']=$login;
-header("location:index.php";
-}
-else {
-	echo "Wrong Username or Password";
-	}
-	}
-	mysql_close($bdd);
-?>
-
 </table>
-
-
-</body>
-
-</html>
